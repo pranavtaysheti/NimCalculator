9b22746a9d08d548c43150e4ee6681f8c4e207a8
Pranav Taysheti <pranavtaysheti@gmail,com>
2022-12-26 15:42:19 +0530

    Cosmetic changes: follow nim conventions

--- src/calculator/token.nim
+++ src/calculator/token.nim
@@ -4,43 +4,40 @@ import constants
 type UnexpectedTokenError = object of CatchableError
 
 type
-  TermType* = enum
-    TTNone, TTNumber, TTSymbol
+  TermType* = enum ttNone, ttNumber, ttSymbol
   
   Term* = object
     case termType*: TermType
-      of TTNone: discard
-      of TTNumber: number*: int
-      of TTSymbol: symbol*: Symbol
+      of ttNone: discard
+      of ttNumber: number*: int
+      of ttSymbol: symbol*: Symbol
 
 proc `$`*(term: Term) : string =
   case term.termType:
-    of TTNone: return ""
-    of TTNumber: return $term.number
-    of TTSymbol: return $term.symbol
+    of ttNone: return ""
+    of ttNumber: return $term.number
+    of ttSymbol: return $term.symbol
 
-type
-  TermStringType = enum
-    TSNone, TSDecimal, TSString
+type TermStringType = enum tsNone, tsDecimal, tsString
 
 proc createTerm(termString: string, termStringType: TermStringType): Term =
   var newTerm: Term
     
   case termStringType:
 
-    of TSNone: newTerm = Term(termType: TTNone)
+    of tsNone: newTerm = Term(termType: ttNone)
 
-    of TSDecimal:
+    of tsDecimal:
       var num: int = 0
       for digit in termString:
         num = num*10 + parseInt($digit)
-      newTerm = Term(termType: TTNumber, number: num)
+      newTerm = Term(termType: ttNumber, number: num)
 
-    of TSString:
+    of tsString:
       var matchedSymbol : Symbol
       try: 
         matchedSymbol = matchNotation(termString)
-        newTerm = Term(termType: TTSymbol, symbol: matchedSymbol)
+        newTerm = Term(termType: ttSymbol, symbol: matchedSymbol)
       except InvalidNotationError: raise
 
   return newTerm
@@ -49,19 +46,19 @@ proc tokenizeExpression*(inputExpression: string) : seq[Term] =
   let rawExpression : string = inputExpression & ' '
   var parsedList : seq[Term]
   var termString = ""
-  var termStringType = TSNone
+  var termStringType = tsNone
 
   for token in rawExpression:
     let newTermStringType =
-      if token.isDigit(): TSDecimal
-      elif token == ' ': TSNone
-      else: TSString
+      if token.isDigit(): tsDecimal
+      elif token == ' ': tsNone
+      else: tsString
 
     if (termStringType != newTermStringType):
 
       try:
         let newTerm = createTerm(termString, termStringType)
-        if newTerm.termType == TTNone: discard
+        if newTerm.termType == ttNone: discard
         else: parsedList.add(newTerm)
         reset(termString)
         
